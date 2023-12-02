4e7cf49b69b061c600fe9af3951d8bc7fa946b9b
spicydll <mason.schmidgall1@gmail.com>
2023-12-02 16:40:52 -0600

    ti -> travelInfo in main

--- traveling_salesman_openmpi.c
+++ traveling_salesman_openmpi.c
@@ -249,7 +249,7 @@ int main(int argc, char *argv[])
                 }
             }
         }
-        gen_perms(travelInfo, starting_tour, 2, ti->best_tour->weight);
+        gen_perms(travelInfo, starting_tour, 2, travelInfo->best_tour->weight);
 
     }
 
