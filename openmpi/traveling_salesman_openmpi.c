202bb85484d6f99d04246ed826e8d78b21bff613
Aaron Baker <crosby.baker2010@gmail.com>
2023-12-04 19:15:09 -0600

    Update traveling_salesman_openmpi.c
    Typo

--- openmpi/traveling_salesman_openmpi.c
+++ openmpi/traveling_salesman_openmpi.c
@@ -186,7 +186,7 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Usage: %s <input file>\n", argv[0]);
-        MPI_finalize();
+        MPI_Finalize();
         exit(1);
     }
 
