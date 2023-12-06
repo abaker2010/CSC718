6e43ca4c39edad885ab16edd7052db7819182bc8
spicydll <mason.schmidgall1@gmail.com>
2023-12-06 17:49:44 -0600

    Upload files from rushmore cluster

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 cd ./openmpi
 
-mpirun -machinefile machinefile.dsu -np 2 ./tsp_openmpi ../test_files/big_test_25
-mpirun -machinefile machinefile.dsu -np 2 ./tsp_openmpi ../test_files/big_test_25
-mpirun -machinefile machinefile.dsu -np 1 ./tsp_openmpi ../test_files/big_test_25
+#mpirun -machinefile machinefile.dsu -np 2 ./tsp_openmpi ../test_files/big_test_25
+#mpirun -machinefile machinefile.dsu -np 2 ./tsp_openmpi ../test_files/big_test_25
+#mpirun -machinefile machinefile.dsu -np 1 ./tsp_openmpi ../test_files/big_test_25
 
 cd ../openmp
 
